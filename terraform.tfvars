f2693a4885002a759fcd1b56dadff5255bdc5729
Nicolas Hommais <68919893+Philomelius@users.noreply.github.com>
2020-07-28 20:48:08 +0200

    A simple Wordpress infrastructure on AWS
    Port 80 and 22 not responding

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+access_key  = "AKIAZPWWOYWQBOTTGAPJ"
+secret_key  = "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"
+region      = "eu-west-3"
---
+++ variables.tf
@@ -0,0 +1,16 @@
+# Wordpress AWS deploy 
+
+variable "access_key" {
+  type          = string
+  description   = "AWS access key"
+}
+
+variable "secret_key" {
+  type          = string
+  description   = "AWS secret key"
+}
+
+variable "region" {
+  type          = string
+  description   = "AWS region"
+}
